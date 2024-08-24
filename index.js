9e242b658d313003c973247ae4f9b88cf2235550
AliAhmed-2 <itz.ambrose10@gmail.com>
2024-08-24 17:32:56 +0500

    created a simple drumkitt

---
+++ index.js
@@ -0,0 +1,50 @@
+
+document.querySelectorAll(".drum")[0].addEventListener('click',getclick0);
+
+
+function getclick0(){
+    var audio=new Audio('sounds/tom-1.mp3');
+    audio.play()
+}
+document.querySelectorAll(".drum")[1].addEventListener('click',getclick1);
+
+
+function getclick1(){
+    var audio=new Audio('sounds/tom-2.mp3');
+    audio.play()
+}
+document.querySelectorAll(".drum")[2].addEventListener('click',getclick2);
+
+
+function getclick2(){
+    var audio=new Audio('sounds/tom-3.mp3');
+    audio.play()
+}
+document.querySelectorAll(".drum")[3].addEventListener('click',getclick3);
+
+
+function getclick3(){
+    var audio=new Audio('sounds/tom-4.mp3');
+    audio.play()
+}
+document.querySelectorAll(".drum")[4].addEventListener('click',getclick4);
+
+
+function getclick4(){
+    var audio=new Audio('sounds/crash.mp3');
+    audio.play()
+}
+document.querySelectorAll(".drum")[5].addEventListener('click',getclick5);
+
+
+function getclick5(){
+    var audio=new Audio('sounds/kick-bass.mp3');
+    audio.play()
+}
+document.querySelectorAll(".drum")[6].addEventListener('click',getclick6);
+
+
+function getclick6(){
+    var audio=new Audio('sounds/snare.mp3');
+    audio.play()
+}
